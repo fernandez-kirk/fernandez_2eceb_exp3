3520fed467c600cc86e3a9aa304057601db14a28
fernandez-kirk <kirkhyohan.fernandez.eng@ust.edu.ph>
2025-09-03 10:41:47 +0800

    Add files via upload

---
+++ Fernandez_Pandas-P1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+PROBLEM 1:
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+cars = pd.read_csv('cars.csv')
+cars
+
+
+# In[6]:
+
+
+cars.head()
+
+
+# In[7]:
+
+
+cars.tail()
+
+
+# In[ ]:
+
+
+
+
